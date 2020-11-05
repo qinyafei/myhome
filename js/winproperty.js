b57b9f914cc37ff47d901b89b8db08e549afe9cb
unknown <qinyf958@163.com>
2020-11-06 00:08:10 +0800

    add property draw

---
+++ js/winproperty.js
@@ -0,0 +1,15 @@
+
+/**
+ * 显示外框属性
+*/
+
+function outFrameProperty(parent) {
+    labelName = document.createElement("label");
+    parent.appendChild(labelName);
+
+    var checkbtn = document.createElement("input");
+    label1 = document.createElement("label");
+    input1 = document.createElement("input");
+    input1.setAttribute("type", "text");
+
+}
